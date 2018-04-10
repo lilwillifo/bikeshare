550ec9dfa2cb351b3957fe7f4e0804aae351450a
Lihkin <shniks@gmail.com>
2018-04-09 21:53:06 -0600

    Added tests for relationships between station and test models.

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 describe Station, type: :model do
-  context 'validations' do
+  context 'Validations' do
     it {should validate_presence_of :name}
     it { should validate_presence_of :dock_count }
     it { should validate_presence_of :city }
     it { should validate_presence_of :installation_date }
+  end
+
+  context "Relationships" do
+    it { is_expected.to have_many(:start_trips) }
+    it { is_expected.to have_many(:end_trips) }
   end
 end
