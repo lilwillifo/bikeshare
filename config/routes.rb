eaa82bc007f9271431c272a14aa46edd37d59a84
Margaret Williford <margaretwilliford4@gmail.com>
2018-04-09 20:25:55 -0600

    users WIP

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   resources :stations, only: [:index]
   resources :conditions, only: %i[index show]
+  resources :users, only: [:new]
+
   get '/login', to: 'sessions#new'
 
   get '/:name', to: 'stations#show'
