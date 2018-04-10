7da47db14fc40a0943012daf97006ce331b4baca
Margaret Williford <margaretwilliford4@gmail.com>
2018-04-09 20:15:00 -0600

    test for visitor creating account

--- spec/features/visitor_can_create_account_spec.rb
+++ spec/features/visitor_can_create_account_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 describe 'As a visitor' do
   context 'when I visit the homepage and click login' do
-    it 'I see a link to create an account' do
+    it 'I can create an account' do
       visit '/'
 
-      expect(page).to have_link('Login')
-    end
-    context 'when I click create account and fill in my credentials' do
-      it 'I should be on the dashboard' do
+      click_link 'Login'
 
-      end
+      expect(current_path).to eq('/login')
+
+      click_link 'Create Account'
+      fill_in 'Username', with: 'TylerRox'
+      fill_in 'Password', with: 'SeCrEtZ'
+      fill_in 'Confirm Password', with: 'SeCrEtZ'
+      click_on 'Submit'
+
+      expect(current_path).to eq ('/dashboard')
+      expect(page).to have_content('Logged in as TylerRox')
+      expect(page).to have_link('Logout')
+      expect(page).to_not have_link('Login')
     end
   end
 end
