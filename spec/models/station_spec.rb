5622cb40782d30fdd30b1b9dcffb7c44beac66f2
Lihkin <shniks@gmail.com>
2018-04-10 13:46:19 -0600

    Cleaning up station spec.

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Station, type: :model do
-  context 'Validations' do
+  context 'validations' do
     it {should validate_presence_of :name}
     it { should validate_presence_of :dock_count }
     it { should validate_presence_of :city }
