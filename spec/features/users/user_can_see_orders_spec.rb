e615c201ec0b4d3666c74b7f0190d3bd54e8901a
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-11 01:13:32 -0600

    user can see orders on dashboard

--- spec/features/users/user_can_see_orders_spec.rb
+++ spec/features/users/user_can_see_orders_spec.rb
@@ -2,16 +2,25 @@ require 'rails_helper'
 
 describe 'User' do
   before(:all) do
-    user = create(:user)
+    DatabaseCleaner.clean
+    @user = create(:user)
     10.times do
       @user.orders.create!
     end
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
   end
 
   scenario 'can see their orders' do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     visit dashboard_path
 
-
+    within('.orders') do
+      10.times do |n|
+        expect(page).to have_content("Order #{n+1}")
+      end
+    end
   end
 end
