dde8f18dbc6a5ffc79a7ef279e135034682c0783
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-10 22:18:43 -0600

    Added other TDD for delete link on show page.

--- spec/features/admin/station/admin_can_delete_station_spec.rb
+++ spec/features/admin/station/admin_can_delete_station_spec.rb
@@ -22,11 +22,18 @@ describe 'admin wants to delete station' do
     end
   end
 
-  # describe 'admin visit station show' do
-  #   it 'to use edit station link' do
-  #     visit "/#{@station.slug}"
-  #
-  #     click_link 'Edit'
-  #   end
-  # end
+  describe 'admin visit station show' do
+    it 'to use edit station link' do
+      visit "/#{@station.slug}"
+
+      click_link 'Delete'
+
+      expect(current_path).to eq(stations_path)
+      expect(page).to have_content('Station Deleted!')
+      expect(page).to_not have_content(@station.name)
+      expect(page).to_not have_content(@station.dock_count)
+      expect(page).to_not have_content(@station.city)
+      expect(page).to_not have_content(@station.installation_date)
+    end
+  end
 end
