91d1c60160fbfb8c2f2c851b9475efc2c82e63c7
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-10 21:46:07 -0600

    Testing to make sure all attributes are still on page.

--- spec/features/admin/station/admin_creates_new_station_spec.rb
+++ spec/features/admin/station/admin_creates_new_station_spec.rb
@@ -13,12 +13,14 @@ describe 'admin visits stations#index' do
       fill_in 'station[name]', with: 'Wookie'
       fill_in 'station[dock_count]', with: '10'
       fill_in 'station[city]', with: 'San Fran'
-      fill_in 'station[installation_date]', with: Time.now
+      fill_in 'station[installation_date]', with: '2018-04-10'
       click_on 'Create Station'
 
       expect(current_path).to eq('/wookie')
-      expect(page).to have_content('Wookie')
       expect(page).to have_content('Wookie created!')
+      expect(page).to have_content('10')
+      expect(page).to have_content('San Fran')
+      expect(page).to have_content('2018-04-10')
     end
   end
 end
