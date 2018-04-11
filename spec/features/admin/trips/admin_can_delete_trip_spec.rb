64a6767875f67120b513850d7ed97722036aed2b
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-11 11:39:32 -0600

    Adding TDD for delete functionality - trips.

---
+++ spec/features/admin/trips/admin_can_delete_trip_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe 'admin wants to delete station' do
+  before(:each) do
+    @admin = create(:admin)
+    @station = create(:station)
+    @trip = create(:trip)
+  end
+
+  describe 'admin visits trips_path' do
+    it 'allows deletion of trip' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+      visit trips_path
+
+      click_link 'Delete'
+
+      expect(current_path).to eq(trips_path)
+      expect(page).to have_content('Trip Deleted!')
+      expect(page).to_not have_content(@trip.duration)
+      expect(page).to_not have_content(@trip.start_date)
+      expect(page).to_not have_content(@trip.end_date)
+      expect(page).to_not have_content(@trip.bike_id)
+      expect(page).to_not have_content(@trip.subscription_type)
+      expect(page).to_not have_content(@trip.zip_code)
+      expect(page).to_not have_content(@trip.start_station_id)
+      expect(page).to_not have_content(@trip.end_station_id)
+    end
+  end
+
+  describe 'admin visits trips_path' do
+    it 'allows use of dlete link from show' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+      visit trip_path(@trip)
+
+      click_link 'Delete'
+
+      expect(current_path).to eq(trips_path)
+      expect(page).to have_content('Trip Deleted!')
+      expect(page).to_not have_content(@trip.duration)
+      expect(page).to_not have_content(@trip.start_date)
+      expect(page).to_not have_content(@trip.end_date)
+      expect(page).to_not have_content(@trip.bike_id)
+      expect(page).to_not have_content(@trip.subscription_type)
+      expect(page).to_not have_content(@trip.zip_code)
+      expect(page).to_not have_content(@trip.start_station_id)
+      expect(page).to_not have_content(@trip.end_station_id)
+    end
+  end
+
+  describe 'as a normal user' do
+    it 'does not allow user to even see link' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit trips_path
+
+      expect(page).to_not have_content('Delete')
+    end
+  end
+
+  describe 'as a visitor' do
+    it 'does not allow user to even see link' do
+      visit trips_path
+
+      expect(page).to_not have_content('Delete')
+    end
+  end
+end
