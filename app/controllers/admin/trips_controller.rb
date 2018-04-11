6a20cc949ace3ca0c8fe9a639440920aa9d1047a
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-11 10:03:40 -0600

    Changes necessary to make test for admin create trip pass.

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -1,3 +1,22 @@
 class Admin::TripsController < Admin::BaseController
-  
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      flash[:success] = 'Trip Created!'
+      redirect_to trip_path(@trip)
+    else
+      flash[:error] = 'Trip Not Created!'
+      render :new
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:duration, :start_date, :end_date, :bike_id, :subscription_type, :zip_code, :start_station_id, :end_station_id)
+  end
 end
--- spec/features/admin/trips/admin_can_create_new_trip_spec.rb
+++ spec/features/admin/trips/admin_can_create_new_trip_spec.rb
@@ -10,18 +10,19 @@ describe 'admin visits trips#index' do
   describe 'admin visit admin_trip#new' do
     it 'allows the admin to create new station' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
-      visit new_admin_station_path
+      visit new_admin_trip_path
 
+      fill_in 'trip[duration]', with: 60
       fill_in 'trip[start_date]', with: '2018-01-09 18:27:55'
       fill_in 'trip[end_date]', with: '2018-01-09 22:27:55'
       fill_in 'trip[bike_id]', with: 8
       fill_in 'trip[subscription_type]', with: 'Premium'
       fill_in 'trip[zip_code]', with: 88888
       fill_in 'trip[start_station_id]', with: 1
-      fill_in 'trip[start_end_id]', with: 2
+      fill_in 'trip[end_station_id]', with: 2
       click_on 'Create Trip'
 
-      expect(current_path).to eq('/wookie')
+      expect(current_path).to eq(trip_path(Trip.all.last))
       expect(page).to have_content('2018-01-09 18:27:55')
       expect(page).to have_content('2018-01-09 22:27:55')
       expect(page).to have_content('8')
