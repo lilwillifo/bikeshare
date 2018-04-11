80911b5c1b615b5afa13ef9655a08d4c4282603a
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-11 07:20:16 -0600

    Adding tests for create station with non-admin.

--- spec/features/admin/station/admin_creates_new_station_spec.rb
+++ spec/features/admin/station/admin_creates_new_station_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe 'admin visits stations#index' do
   before(:each) do
     @admin = create(:admin)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
   end
 
   describe 'admin visit admin_station#new' do
     it 'allows the admin to create new station' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
       visit new_admin_station_path
 
       fill_in 'station[name]', with: 'Wookie'
@@ -21,6 +21,26 @@ describe 'admin visits stations#index' do
       expect(page).to have_content('10')
       expect(page).to have_content('San Fran')
       expect(page).to have_content('2018-04-10')
+    end
+  end
+
+  describe 'as normal user' do
+    it 'does not allow default user to see delete link' do
+      user = User.create(username: 'fern@gully.com',
+                         password: 'password')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit new_admin_station_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+
+  describe 'as visitor' do
+    it 'does not allow visitor to see delete link' do
+
+      visit new_admin_station_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
 end
