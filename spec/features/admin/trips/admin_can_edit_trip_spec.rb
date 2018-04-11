70bf3baade9027295fff0e717605cf0cef891bc1
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-11 11:21:21 -0600

    Added testing for edit links on trip index and show.

--- spec/features/admin/trips/admin_can_edit_trip_spec.rb
+++ spec/features/admin/trips/admin_can_edit_trip_spec.rb
@@ -33,4 +33,62 @@ describe 'admin visits trips#edit' do
       expect(page).to have_content('2')
     end
   end
+
+  describe 'admin visit trips_path' do
+    it 'to use edit trip link' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+      visit trips_path
+
+      click_link 'Edit'
+
+      fill_in 'trip[duration]', with: 60
+      fill_in 'trip[start_date]', with: '2018-01-09 18:27:55'
+      fill_in 'trip[end_date]', with: '2018-01-09 22:27:55'
+      fill_in 'trip[bike_id]', with: 8
+      fill_in 'trip[subscription_type]', with: 'Premium'
+      fill_in 'trip[zip_code]', with: 88888
+      fill_in 'trip[start_station_id]', with: 1
+      fill_in 'trip[end_station_id]', with: 2
+      click_on 'Update Trip'
+
+      expect(current_path).to eq(trip_path(@trip))
+      expect(page).to have_content('2018-01-09 18:27:55')
+      expect(page).to have_content('2018-01-09 22:27:55')
+      expect(page).to have_content('8')
+      expect(page).to have_content('Premium')
+      expect(page).to have_content('88888')
+      expect(page).to have_content('1')
+      expect(page).to have_content('2')
+    end
+  end
+
+  describe 'admin visit trips_path' do
+    it 'to use edit trip link' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+      visit trip_path(@trip)
+
+      click_link 'Edit'
+
+      fill_in 'trip[duration]', with: 60
+      fill_in 'trip[start_date]', with: '2018-01-09 18:27:55'
+      fill_in 'trip[end_date]', with: '2018-01-09 22:27:55'
+      fill_in 'trip[bike_id]', with: 8
+      fill_in 'trip[subscription_type]', with: 'Premium'
+      fill_in 'trip[zip_code]', with: 88888
+      fill_in 'trip[start_station_id]', with: 1
+      fill_in 'trip[end_station_id]', with: 2
+      click_on 'Update Trip'
+
+      expect(current_path).to eq(trip_path(@trip))
+      expect(page).to have_content('2018-01-09 18:27:55')
+      expect(page).to have_content('2018-01-09 22:27:55')
+      expect(page).to have_content('8')
+      expect(page).to have_content('Premium')
+      expect(page).to have_content('88888')
+      expect(page).to have_content('1')
+      expect(page).to have_content('2')
+    end
+  end
 end
