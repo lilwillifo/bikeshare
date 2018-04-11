7d572f4673e2506e6b4a1db6558f0ce9af2eeade
Lihkin <shniks@gmail.com>
2018-04-10 18:45:29 -0600

    Created feature test for bike-shop accessories.

---
+++ spec/features/visitors/trips/visitor_can_see_bike-shop_accessories_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'As a Visitor' do
+  context 'When I visit '/bike-shop'' do
+    before(:each) do
+      @accessories = create_list(:item, 12)
+    end
+    scenario 'I see at least 12 bike accessories for sale' do
+      visit '/bike-shop'
+
+      @accessories.each do |accessory|
+        expect(page).to have_content(accessory.title)
+        expect(page).to have_content(accessory.description)
+        expect(page).to have_content(accessory.price)
+      end
+    end
+  end
+end
