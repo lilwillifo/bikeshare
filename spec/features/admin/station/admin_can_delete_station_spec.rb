a881f07482eb9ee7ad0bc34757bb669a223e425a
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-10 22:17:45 -0600

    Adding TDD for delete links.

---
+++ spec/features/admin/station/admin_can_delete_station_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'admin wants to delete station' do
+  before(:each) do
+    @admin = create(:admin)
+    @station = create(:station)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+  end
+
+  describe 'admin visit stations_path' do
+    it 'to use delete station link' do
+      visit stations_path
+
+      click_link 'Delete'
+
+      expect(current_path).to eq(stations_path)
+      expect(page).to have_content('Station Deleted!')
+      expect(page).to_not have_content(@station.name)
+      expect(page).to_not have_content(@station.dock_count)
+      expect(page).to_not have_content(@station.city)
+      expect(page).to_not have_content(@station.installation_date)
+    end
+  end
+
+  # describe 'admin visit station show' do
+  #   it 'to use edit station link' do
+  #     visit "/#{@station.slug}"
+  #
+  #     click_link 'Edit'
+  #   end
+  # end
+end
