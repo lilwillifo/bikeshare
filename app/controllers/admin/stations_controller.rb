f1385b125c604837489e9c767c7cc497c9d02b4b
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-10 22:11:57 -0600

    Working with slugs, fun. Added edit functionality, tested edit link on index page.

--- app/controllers/admin/stations_controller.rb
+++ app/controllers/admin/stations_controller.rb
@@ -1,4 +1,10 @@
 class Admin::StationsController < Admin::BaseController
+  before_action :set_station, except: [:new, :create]
+
+  def edit
+    # @station = Station.friendly.find(params[:name])
+  end
+
   def new
     @station = Station.new
   end
@@ -14,9 +20,25 @@ class Admin::StationsController < Admin::BaseController
     end
   end
 
+  def update
+    @station.slug = nil
+    @station.update(station_params)
+    if @station.save
+      flash[:success] = "#{@station.name} updated!"
+      redirect_to "/#{@station.slug}"
+    else
+      flash[:error] = 'Station not updated!'
+      render :edit
+    end
+  end
+
   private
 
   def station_params
     params.require(:station).permit(:name, :dock_count, :city, :installation_date)
+  end
+
+  def set_station
+    @station = Station.friendly.find(params[:id])
   end
 end
