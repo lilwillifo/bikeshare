65fe430488705cce765a215166e8f3c24bafb4c9
Margaret <33872479+lilwillifo@users.noreply.github.com>
2018-04-12 13:02:57 -0600

    Update station.rb

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,5 +4,5 @@ class Station < ApplicationRecord
 
   validates_presence_of :name, :dock_count, :city, :installation_date
   has_many :start_trips, class_name: "Trip", foreign_key: "start_station_id", dependent: :destroy
-  has_many :end_trips, class_name: "Trip", foreign_key: "end_station_id"
+  has_many :end_trips, class_name: "Trip", foreign_key: "end_station_id", dependent: :destroy
 end
