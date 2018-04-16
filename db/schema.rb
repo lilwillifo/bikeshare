2ef5a849d5da5245c061aac1ffb75d3a43d98cda
Margaret Williford <margaretwilliford4@gmail.com>
2018-04-16 08:19:19 -0600

    running migration

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20180416022846) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.float "total"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
