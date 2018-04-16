adaf240578023f8112fa1f3e6ff6412d3d9bd98b
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-16 15:17:54 -0600

    Most frequent destination/origin stations using select

--- spec/features/users/user_can_see_extra_station_info_spec.rb
+++ spec/features/users/user_can_see_extra_station_info_spec.rb
@@ -4,21 +4,42 @@ describe 'As a user/admin' do
   before(:each) do
     DatabaseCleaner.clean
     @user = create(:user)
-    @station = create(:station)
-    @trips_from_station = create_list(:trip, 3, start_station: @station)
-    @trips_end_station = create_list(:trip, 3, end_station: @station)
+    @station1 = create(:station)
+    @station2 = create(:station)
+    @station3 = create(:station)
+
+    create(:condition)
+    @trips_from_station = create_list(:trip, 3, start_station: @station1, end_station: @station2)
+    create_list(:trip, 5, start_station: @station3, end_station: @station1)
+    @trips_end_station = create_list(:trip, 4, start_station: @station2, end_station: @station1)
   end
 
   after(:each) do
     DatabaseCleaner.clean
   end
 
-  context 'when I visit a station show page' do
+  scenario 'when I visit a station show page' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-    visit "/#{@station.slug}"
+    visit "/#{@station1.slug}"
 
-    expect(page).to have_content("Trips from here: #{@station.start_trips.length}")
-    expect(page).to have_content("Trips to here: #{@station.end_trips.length}")
+    expect(page).to have_content("Trips from here: #{@station1.start_trips.length}")
+    expect(page).to have_content("Trips to here: #{@station1.end_trips.length}")
+
+    expect(page).to have_content("Most frequent destination: #{@station1.most_frequent_destination.name}")
+    expect(page).to have_link(@station1.most_frequent_destination.name)
+
+    click_on @station1.most_frequent_destination.name
+    expect(current_path).to eq("/#{@station1.most_frequent_destination.slug}")
+
+    visit "/#{@station1.slug}"
+
+    expect(page).to have_content("Most frequent origin: #{@station1.most_frequent_origin.name}")
+    expect(page).to have_link(@station1.most_frequent_origin.name)
+
+    click_on @station1.most_frequent_origin.name
+    expect(current_path).to eq("/#{@station1.most_frequent_origin.slug}")
+
+    visit "/#{@station1.slug}"
   end
 end
